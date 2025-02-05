81eaa27e01e1d5dfdc5ab54341aa0d90a59fd606
MatrixEditor <58256046+MatrixEditor@users.noreply.github.com>
2025-02-05 20:42:51 +0100

    Dex file structs (incomplete) ---
    + Header
    + various items

---
+++ src/file/header.rs
@@ -0,0 +1,115 @@
+#[repr(C)]
+#[derive(Debug)]
+pub struct Header {
+    /// magic value
+    magic: [u8; 8],
+
+    /// Taken from Android docs:
+    ///
+    /// Adler32 checksum of the rest of the file (everything but `magic` and this
+    /// field); used to detect file corruption.
+    pub checksum: u32,
+
+    /// Android docs:
+    ///
+    /// SHA-1 signature (hash) of the rest of the file (everything but `magic`,
+    /// `checksum`, and this field); used to uniquely identify files.
+    signature: [u8; 20],
+
+    /// Size of the entire file including the header.
+    pub file_size: u32,
+
+    /// Size of the header (this struct), in bytes. It is always 0x70.
+    pub header_size: u32,
+
+    /// Endian contant - ART source code only supports one byte order
+    pub endian_tag: u32,
+
+    // unused {
+    /// size of the link section, or 0 if this file isn't statically linked
+    pub link_size: u32,
+
+    /// offset from the start of the file to the link section, or `0` if
+    /// `link_size == 0`. The offset, if non-zero, should be to an offset
+    /// into the `link_data` section.
+    pub link_off: u32,
+    // } unused
+    /// offset from the start of the file to the map item. The offset, which
+    /// must be non-zero, should be to an offset into the `data` section.
+    pub map_off: u32,
+
+    /// count of strings in the string identifiers list
+    pub string_ids_size: u32,
+
+    /// offset from the start of the file to the string identifiers list, or
+    /// `0` if `string_ids_size == 0`.
+    pub string_ids_off: u32,
+
+    /// count of elements in the type identifiers list, at most `65535`
+    pub type_ids_size: u32,
+
+    /// offset from the start of the file to the type identifiers list, or
+    /// `0` if `type_ids_size == 0`.
+    pub type_ids_off: u32,
+
+    /// count of elements in the proto identifiers list, at most `65535`
+    pub proto_ids_size: u32,
+
+    /// offset from the start of the file to the proto identifiers list, or
+    /// `0` if `proto_ids_size == 0`.
+    pub proto_ids_off: u32,
+
+    /// count of elements in the field identifiers list
+    pub field_ids_size: u32,
+
+    /// offset from the start of the file to the field identifiers list, or
+    /// `0` if `field_ids_size == 0`.
+    pub field_ids_off: u32,
+
+    /// count of elements in the method identifiers list
+    pub method_ids_size: u32,
+
+    /// offset from the start of the file to the method identifiers list, or
+    /// `0` if `method_ids_size == 0`.
+    pub method_ids_off: u32,
+
+    /// count of elements in the class definitions list
+    pub class_defs_size: u32,
+
+    /// offset from the start of the file to the class definitions list, or
+    /// `0` if `class_defs_size == 0`.
+    pub class_defs_off: u32,
+
+    /// size of the data section (in bytes)
+    pub data_size: u32,
+
+    /// offset from the start of the file to the data section
+    pub data_off: u32,
+}
+
+unsafe impl plain::Plain for Header {}
+
+impl Header {
+    pub fn get_magic(&self) -> &[u8; 8] {
+        &self.magic
+    }
+
+    pub fn get_signature(&self) -> &[u8; 20] {
+        &self.signature
+    }
+
+    pub fn get_version(&self) -> u32 {
+        let version_raw = &self.magic[4..7];
+        String::from_utf8_lossy(version_raw)
+            .parse()
+            .unwrap_or_default() // will lead to invalid dex file
+    }
+}
+
+#[repr(C)]
+#[derive(Debug)]
+pub struct HeaderV41 {
+    pub inner: Header,
+    pub container_size: u32, // total size of all dex files in the container.
+    pub header_off: u32,     // offset of this dex's header in the container.
+}
---
+++ src/file/structs.rs
@@ -0,0 +1,181 @@
+use plain::Plain;
+
+pub type StringIndex = u32;
+
+#[repr(C)]
+#[derive(Debug)]
+pub struct StringId {
+    pub string_data_off: u32,
+}
+
+unsafe impl plain::Plain for StringId {}
+
+impl StringId {
+    #[inline]
+    pub const fn offset(&self) -> usize {
+        self.string_data_off as usize
+    }
+}
+
+pub type TypeIndex = u16;
+
+#[repr(C)]
+#[derive(Debug)]
+pub struct TypeId {
+    pub descriptor_idx: StringIndex,
+}
+
+unsafe impl plain::Plain for TypeId {}
+
+#[repr(C)]
+#[derive(Debug)]
+pub struct FieldId {
+    pub class_idx: TypeIndex,  // index into type_ids_ array for defining class
+    pub type_idx: TypeIndex,   // index into type_ids_ array for field type
+    pub name_idx: StringIndex, // index into string_ids_ array for field name
+}
+
+unsafe impl plain::Plain for FieldId {}
+
+pub type ProtoIndex = u16;
+
+#[repr(C)]
+#[derive(Debug)]
+pub struct ProtoId {
+    pub shorty_idx: StringIndex, // index into string_ids array for shorty descriptor
+    pub return_type_idx: TypeIndex, // index into type_ids array for return type
+    pad_: u16,                   // padding = 0
+    pub parameters_off: u32,     // file offset to type_list for parameter types
+}
+
+unsafe impl plain::Plain for ProtoId {}
+
+#[repr(C)]
+#[derive(Debug)]
+pub struct MethodId {
+    pub class_idx: TypeIndex,  // index into type_ids_ array for defining class
+    pub proto_idx: ProtoIndex, // index into proto_ids_ array for method signature
+    pub name_idx: StringIndex, // index into string_ids_ array for method name
+}
+
+unsafe impl plain::Plain for MethodId {}
+
+#[repr(C)]
+#[derive(Debug)]
+pub struct ClassDef {
+    pub class_idx: TypeIndex, // index into type_ids_ array for this class
+    pad1_: u16,               // padding = 0
+    pub access_flags: u32,
+    pub superclass_idx: TypeIndex, // index into type_ids_ array for superclass
+    pad2_: u16,                    // padding = 0
+    pub interfaces_off: u32,       // file offset to TypeList
+    pub source_file_idx: StringIndex, // index into string_ids_ for source file name
+    pub annotations_off: u32,      // file offset to annotations_directory_item
+    pub class_data_off: u32,       // file offset to class_data_item
+    pub static_values_off: u32,    // file offset to EncodedArray
+}
+
+unsafe impl plain::Plain for ClassDef {}
+
+#[repr(C)]
+#[derive(Debug)]
+pub struct TypeItem {
+    pub type_idx: TypeIndex, // index into type_ids section
+}
+
+unsafe impl plain::Plain for TypeItem {}
+
+pub type TypeList<'a> = &'a [TypeItem];
+
+#[repr(C)]
+#[derive(Debug)]
+pub struct MapItem {
+    pub type_: MapItemType,
+    unused_: u16,
+    pub size: u32,
+    pub off: u32,
+}
+
+unsafe impl plain::Plain for MapItem {}
+
+pub type MapList<'a> = &'a [MapItem];
+
+#[repr(u16)]
+#[derive(Debug)]
+pub enum MapItemType {
+    HeaderItem = 0x0000,
+    StringIdItem = 0x0001,
+    TypeIdItem = 0x0002,
+    ProtoIdItem = 0x0003,
+    FieldIdItem = 0x0004,
+    MethodIdItem = 0x0005,
+    ClassDefItem = 0x0006,
+    CallSiteIdItem = 0x0007,
+    MethodHandleItem = 0x0008,
+    MapList = 0x1000,
+    TypeList = 0x1001,
+    AnnotationSetRefList = 0x1002,
+    AnnotationSetItem = 0x1003,
+    ClassDataItem = 0x2000,
+    CodeItem = 0x2001,
+    StringDataItem = 0x2002,
+    DebugInfoItem = 0x2003,
+    AnnotationItem = 0x2004,
+    EncodedArrayItem = 0x2005,
+    AnnotationsDirectoryItem = 0x2006,
+    HiddenapiClassData = 0xF000,
+}
+
+#[repr(C)]
+#[derive(Debug)]
+pub struct MethodHandleItem {
+    pub method_handle_type: TypeIndex,
+    reserved1_: u16,
+    pub field_or_method_idx: u16, // Field index for accessors, method index otherwise.
+    reserved2_: u16,
+}
+
+unsafe impl plain::Plain for MethodHandleItem {}
+
+#[repr(C)]
+#[derive(Debug)]
+pub struct CallSiteIdItem {
+    pub data_off: u32, // Offset into data section pointing to encoded array items.
+}
+
+unsafe impl plain::Plain for CallSiteIdItem {}
+
+#[repr(C)]
+#[derive(Debug)]
+pub struct HiddenapiClassData<'a> {
+    pub size: u32,
+    flags_offset: &'a [u8],
+}
+
+impl<'a> HiddenapiClassData<'a> {
+    pub fn get_flags_slice(&self, class_def_idx: u32) -> Option<&'a [u8]> {
+        let offset = (class_def_idx * 4) as usize;
+        match u32::from_bytes(&self.flags_offset[offset..]) {
+            Ok(0) => None,
+            // offset starts from beginning of this object
+            Ok(start) => Some(&self.flags_offset[(*start - 4) as usize..]),
+            _ => None,
+        }
+    }
+}
+
+unsafe impl<'a> plain::Plain for HiddenapiClassData<'a> {}
+
+#[repr(C)]
+#[derive(Debug)]
+pub struct CodeItem {
+    pub registers_size: u16,
+    pub ins_size: u16,
+    pub outs_size: u16,
+    pub tries_size: u16,
+    pub debug_info_off: u32,
+    pub insns_size: u32,
+}
+
+
+unsafe impl plain::Plain for CodeItem {}
